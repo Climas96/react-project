498f42b2c37465b60e8d4aacbd4fd1bb37e1963a
Erik Climas Samano <climaxx98@gmail.com>
2026-04-07 17:56:13 -0600

    se actualiza app
    prueba de commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const [mostrar,setMostrar] = useState(true);
     
         return (
           <>
+          <h1>Prueba branch</h1>
           <input type="checkbox" defaultChecked={mostrar} onChange={e => setMostrar(e.target.checked)}/> Mostrar componente
 
           {mostrar ? <EjemploUseEffect/> : undefined}
